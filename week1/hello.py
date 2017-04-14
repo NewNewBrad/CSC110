ebe6a124fe6422705ce59ed9c696e20becb9210b
Brad Jones <bradford.jones@gmail.com>
2017-04-14 03:22:29 -0700

    Week 1 resources

---
+++ week1/hello.py
@@ -0,0 +1,13 @@
+# CSC 110 Examples
+# Program to prompt the user for their name, then say hello to the user
+
+
+# input section
+name = input('Please enter your name: ')
+fav_color = input("What's your favorite color? ")
+
+# output section
+print ("hello", name, ".  Nice to meet you.")
+print ("Your favorite color is", fav_color)
+print ("Mine is magenta")
+
