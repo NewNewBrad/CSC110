25ac3a3a7e0d3799ae0cae3d8ad344db2d35c515
Brad <Bradford.jones@gmail.com>
2017-04-13 18:07:50 -0700

    Created HW2 file and directory

---
+++ week2/Lab2/JonesLab2_1_1.py
@@ -0,0 +1,11 @@
+# Brad Jones
+# CSC 110 - Section 6
+# 4/12/2017
+
+#Farenheight to Celsius Converter
+print("Brad Jones - Farenheight to Celsius")
+tempF = float(input("Please enter the temperature(F): "));
+tempC = (tempF - 32) *  (5.0 / 9.0)
+print ("The current temperature is: ")
+print (tempF, "F")
+print (tempC, "C")
---
+++ week2/Lab2/JonesLab2_1_2.py
@@ -0,0 +1,8 @@
+#Brad Jones
+#CSC111 - Section 6
+#4/12/2017
+
+#Calculating the sum of a numbers digits
+
+Num1 = int(input("Please type any 3-digit number: "))
+print(Num1)
