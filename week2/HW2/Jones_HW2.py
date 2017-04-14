2b02c2cb54c33908dec375125987f9842daee5f4
Brad <Bradford.jones@gmail.com>
2017-04-13 18:14:34 -0700

    Pasted Example I/O

---
+++ week2/HW2/Jones_HW2.py
@@ -0,0 +1,49 @@
+#Brad Jones
+#CSC110 - Section 6
+#4/13/2017
+
+# Home Renovation Cost Calculator
+
+###############Example for testing####################
+
+#INPUT
+#Enter a length of house in feet: 		50
+#Enter a width of house in feet: 		30
+#Enter a height of house in feet: 		10
+#Enter a cost of profile in $: 			128.75
+#Enter a cost of siding nails in $: 		15.96
+#Enter a cost of side strips in $: 		11
+#Enter a cost of a bundle of shingles in $: 	24.75
+#Enter a cost of roofing nails in $: 		2.98
+#
+#OUTPUT
+#The house has length = 50, width = 30, height = 10
+#SIDING
+#******************************************
+#Total area of walls is 			1600
+#Total profile is 				80
+#Boxes of siding nails are 		40
+#Weather side strips are 		54
+#The cost of materials for siding	$11532.40
+#******************************************
+#ROOF
+#******************************************
+#Bundles of shingles are 		93
+#Boxes of roofing nails are 		92.38
+#******************************************
+#TOTAL
+#*******************************************
+#The cost of materials for the roof	$2394.13
+#Total cost of home renovation	 	$13926.53
+
+#######################################################
+
+#USER INPUT
+#Enter a length of house in feet:
+#Enter a width of house in feet:
+#Enter a height of house in feet:
+#Enter a cost of profile in $:
+#Enter a cost of siding nails in $:
+#Enter a cost of side strips in $:
+#Enter a cost of a bundle of shingles in $:
+#Enter a cost of roofing nails in $:
