0eb6c5b23e120faec41e1c788c7c7de8f2ebafea
Brad Jones <bradford.jones@gmail.com>
2017-05-28 16:39:45 -0700

    completed HW7

--- week8/HW7/Jones_HW7.py
+++ week8/HW7/Jones_HW7.py
@@ -11,7 +11,7 @@ def main():
         line = line.rstrip('\n')
         year = line
 
-        # File output
+        # File output  header info
         outputFile = open('REPORT - ' + year + '.txt', 'w')
         outputFile.write('*' * 63)
         outputFile.write('\n')
@@ -21,31 +21,30 @@ def main():
         outputFile.write("Month \t Maximum \t Minimum \t Average \t Rain \n")
         outputFile.write('\n')
 
-        #Data accumalators
-        maxTemp_year = 0
-        minTemp_year = 999
-        maxrainDays_year = 0
-        minrainDays_year = 999
-        months = []
-        maxTemps = []
-        minTemps = []
-        rainDays = []
-        avgTemp = []
-        monthIndex = 0
+        #List accumalators
+        months = []         #Names of Months
+        maxTemps = []       #Highest temp in given month
+        minTemps = []       #Lowest temp in given month
+        rainDays = []       #Number of rainy days in given month
+        avgTemp = []        #Average temp of given month
+        monthIndex = 0      #Tracks number of Months processed.
 
         while line != '':
             #Pull the name of the month from the file and update to Months list
             line = weatherData.readline()
             line = line.rstrip('\n')
-            if line == '': #This is an ugly way to fix a problem I dont understand. The loop was going 1 extra time before this, returning '' err
+
+            # This is an ugly way to fix a problem I dont understand. The loop was going 1 extra time before this, returning '' err
+            if line == '':
                 break
+
+            #Add month to Months[]
             months.append(line)
-            #print(months)
 
             #Read the number of Days in order to set the range of the for loop
             days = int(weatherData.readline())
 
-            #Collects temps and number of rain days for a single month
+            #monthsTemp[] is a list of all temps recorded during the CURRENT month
             monthsTemp = []
             rainDays_mo = 0
 
@@ -59,70 +58,53 @@ def main():
                 if line == "1":
                     rainDays_mo += 1
 
-            #Sort list of temps for 1 month only
+            #Sorts monthsTemp[]
             #Months[i] will match with minTemp[i], maxTemp[i], rainDays[i] and avgTemp[i]
             bubbleSort(monthsTemp)
             minTemp_mo = int(monthsTemp[0])
             maxTemp_mo = int(monthsTemp[len(monthsTemp)-1])
-            current_mo = str(months[monthIndex])
-            print(current_mo)
 
-
-            #Add min temp of each month to the minTemp list
+            #Add min temp of each month to the minTemp[]
             minTemps.append(minTemp_mo)
-            #Compare the min temp of current month to min temp of year so far
-            minTemp_year = is_min(minTemp_year, minTemp_mo)
 
-            #if current min temp is also minTemp for the year, track which month
-            if minTemp_year == minTemp_mo:
-                minTemp_year_mo = current_mo
-
-
-            #Add max temp of each month to the maxTemp list
+            #Add max temp of each month to the maxTemp[]
             maxTemps.append(maxTemp_mo)
-            #Compare the max temp of current month to max temp of year so far
-            maxTemp_year = is_max(int(maxTemp_year), maxTemp_mo)
 
-            #if current max temp is also maxTemp for the year, track which month
-            if maxTemp_year == maxTemp_mo:
-                maxTemp_year_mo = current_mo
-
-
+            #Add number of rain days of each month to rainDays[]
             rainDays.append(rainDays_mo)
-            maxrainDays_year = is_max(maxrainDays_year, rainDays_mo)
-            if maxrainDays_year == rainDays_mo:
-                maxTemp_year_mo = months[monthIndex]
 
+            #Converts temp list to interger, finds sum, divides by # of days in month
+            #to find Average temp of each month
+            sumOftemps = sum(int(i) for i in monthsTemp)
 
-            minrainDays_year = is_min(minrainDays_year, rainDays_mo)
-            if minrainDays_year == rainDays_mo:
-                minrainDays_year_mo = months[monthIndex]
-            #print(rainDays_mo)
-
-
-
-
-
-            sumOftemps = sum(int(i) for i in monthsTemp) #converts list items to int
+            #Add average temp of each month to avgTemp[]
             avgTemp.append(format((sumOftemps/days), '.2f'))
-            #print(avgTemp)
 
+            #File ouput info. Each loop prints new line
             outputFile.write("{:<11} {:<14} {:<14} {:<14} {:<11}\n".format(months[monthIndex], maxTemps[monthIndex], minTemps[monthIndex], avgTemp[monthIndex], rainDays[monthIndex]))
+
+            #Increment to next month loop
             monthIndex += 1
 
+        #Close files
         outputFile.close()
         weatherData.close()
-        print(months[0])
-        print("The maximum temperature of {} was in {}".format(maxTemp_year, maxTemp_year_mo))
-        print("The minimum temperature of {} was in {}".format(minTemp_year, minTemp_year_mo))
 
+        #Console Output
+        maxTempsIndex = maxTemps.index(max(maxTemps))
+        print("The maximum temperature for the year was {} in {}".format(max(maxTemps), months[maxTempsIndex]))
+        minTempsIndex = minTemps.index(min(minTemps))
+        print("The minimum temperature for the year was {} in {}".format(min(minTemps), months[minTempsIndex]))
+        maxRainIndex = rainDays.index(max(rainDays))
+        print("The maximum number of rainy days was {} in {}".format(max(rainDays), months[maxRainIndex]))
+        minRainIndex = rainDays.index(min(rainDays))
+        print("The minimum number of rainy days was {} in {}".format(min(rainDays), months[minRainIndex]))
 
     except ValueError as err:
         print(err)
 
     except IOError as err:
         print(err)
-
 
 #Sorting a list
 def bubbleSort(list):
@@ -132,23 +114,5 @@ def bubbleSort(list):
                 temp = list[i]
                 list[i] = list[i + 1]
                 list[i + 1] = temp
-
-
-#Compares 2 values and returns the smaller
-def is_min(minValue, currentValue):
-    #print('running is_min()...')
-    if currentValue <= minValue:
-        return currentValue
-    else:
-        return minValue
-
-
-#Compares 2 values and returns the larger
-def is_max(maxValue, currentValue):
-    #print('running is_max()')
-    if currentValue >= maxValue:
-        return currentValue
-    else:
-        return maxValue
 
 main()
